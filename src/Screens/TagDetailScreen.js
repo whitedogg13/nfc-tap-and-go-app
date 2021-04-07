9284a3e6a8170f48eb489c5881fa8d72cd42a815
Richie <whitedogg13@gmail.com>
2021-04-07 11:22:22 +0800

    parse ndef

--- src/Screens/TagDetailScreen.js
+++ src/Screens/TagDetailScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Ndef} from 'react-native-nfc-manager';
 
 function TagDetailScreen(props) {
   const {route} = props;
   const {tag} = route.params;
+  let uri = null;
+
+  if (tag.ndefMessage) {
+    const ndefRecord = tag.ndefMessage[0];
+    if (ndefRecord.tnf === Ndef.TNF_WELL_KNOWN) {
+      if (ndefRecord.type.every((b, i) => b === Ndef.RTD_BYTES_URI[i])) {
+        uri = Ndef.uri.decodePayload(ndefRecord.payload);
+      }
+    }
+  }
+
   return (
     <View style={styles.wrapper}>
-      <Text>{JSON.stringify(tag, null, 2)}</Text>
+      {uri ? <Text>{uri}</Text> : <Text>{JSON.stringify(tag, null, 2)}</Text>}
     </View>
   );
 }
